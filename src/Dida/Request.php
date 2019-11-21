a6738d9c076b1b70512bc72c74e22e1526d2c4c9
Macc Liu <maccliu@qq.com>
2019-11-21 19:10:20 +0800

    修改版权声明

--- src/Dida/Request.php
+++ src/Dida/Request.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Dida Framework  -- PHP轻量级快速开发框架
- * 版权所有 (c) 2017-2019 上海宙品信息科技有限公司
+ * 版权所有 (c) 上海宙品信息科技有限公司
  *
  * 官网: <https://github.com/zeupin/dida>
  * Gitee: <https://gitee.com/zeupin/dida>
