63e096af49244cebfd4fa58d094542220d1e76c9
Liu Nian <maccliu@qq.com>
2020-06-24 22:57:37 +0800

    app配置

---
+++ conf/app.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Dida Framework  -- PHP轻量级快速开发框架
+ * 版权所有 (c) 上海宙品信息科技有限公司
+ *
+ * Github: <https://github.com/zeupin/dida>
+ * Gitee: <https://gitee.com/zeupin/dida>
+ */
+
+return [
+    // 默认时区
+    'default_timezone' => 'Asia/Shanghai',
+];
