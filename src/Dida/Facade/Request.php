1521064059ecf00dbf9784b8663e94c8f3d97128
Liu Nian <maccliu@qq.com>
2020-06-24 16:28:27 +0800

    静态转 Facade

---
+++ src/Dida/Facade/Request.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Dida Framework  -- PHP轻量级快速开发框架
+ * 版权所有 (c) 上海宙品信息科技有限公司
+ *
+ * Github: <https://github.com/zeupin/dida>
+ * Gitee: <https://gitee.com/zeupin/dida>
+ */
+
+namespace Dida\Facade;
+
+use \Dida\Facade;
+
+class Request extends Facade
+{
+}
