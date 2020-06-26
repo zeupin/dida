242e364c211f06b54338d45c4c3ce82faeb9b92b
Liu Nian <maccliu@qq.com>
2020-06-27 02:11:29 +0800

    新增 `ServiceBus` 服务总线

---
+++ src/Dida/ServiceBus.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Dida Framework  -- PHP轻量级快速开发框架
+ * 版权所有 (c) 上海宙品信息科技有限公司
+ *
+ * Github: <https://github.com/zeupin/dida>
+ * Gitee: <https://gitee.com/zeupin/dida>
+ */
+
+namespace Dida;
+
+/**
+ * 服务总线
+ */
+class ServiceBus
+{
+}
