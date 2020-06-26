045f24b8e66b2a9e1186513029cea71293be18fe
Liu Nian <maccliu@qq.com>
2020-06-27 02:45:18 +0800

    `Dida\EventBus` ready

--- src/Dida/EventBus.php
+++ src/Dida/EventBus.php
@@ -9,15 +9,22 @@
 
 namespace Dida;
 
+use \Dida\Event\EventException;
+
 /**
  * EventBus 事件总线
  */
-final class EventBus
+class EventBus
 {
     /**
-     * Version
+     * 版本号
      */
-    const VERSION = '20171124';
+    const VERSION = '20200627';
+
+    /**
+     * 错误代码
+     */
+    const ERROR_EVENT_NOT_FOUND = 1000;
 
     /*
      * 所有已经登记的事件
@@ -25,25 +32,26 @@ final class EventBus
      *
      * @var array
      */
-    protected $events = [];
+    protected static $events = [];
 
     /*
      * 所有已经登记的事件回调函数
      * $hooks["event_name"][id] = [$callback, $parameters]
      */
-    protected $hooks = [];
+    protected static $hooks = [];
 
     /**
      * 新增一个event。
      *
      * @param string $event 事件名称
+     *
+     * @return void
      */
-    public function add($event)
+    public static function addEvent($event)
     {
         // 如果事件不存在，则创建之
-        if (!isset($this->events[$event])) {
-            $this->events[$event] = true;
-            return true;
+        if (!isset(self::$events[$event])) {
+            self::$events[$event] = true;
         }
     }
 
@@ -52,21 +60,23 @@ final class EventBus
      *
      * @param string $event 事件名称
      *
-     * @return bool 删除用户事件是否完成。
+     * @return bool 删除事件是否完成。
      */
-    public function remove($event)
+    public static function removeEvent($event)
     {
-        unset($this->events[$event], $this->hooks[$event]);
+        unset(self::$events[$event], self::$hooks[$event]);
     }
 
     /**
      * 检查一个事件是否已经存在。
      *
      * @param string $event 事件名称
+     *
+     * @return bool
      */
-    public function has($event)
+    public static function hasEvent($event)
     {
-        return isset($this->events[$event]);
+        return isset(self::$events[$event]);
     }
 
     /**
@@ -75,25 +85,26 @@ final class EventBus
      * @param string   $event      事件名称
      * @param callback $callback   回调函数
      * @param array    $parameters 回调函数的参数
-     * @param string   $id         设置这个回调函数的id
+     * @param string   $id         指定回调函数的id
      *
-     * @return $this
-     *
-     * @throws EventException
+     * @return int 成功返回0,失败返回错误代码.
      */
-    public function hook($event, $callback, array $parameters = [], $id = null)
+    public static function hook($event, $callback, array $parameters = [], $id = null)
     {
-        if ($this->has($event)) {
+        if (self::hasEvent($event)) {
+            // 如果不指定id, 则callback放到队列尾部
+            // 如果指定了id, 则用新的callback代替旧的callback
             if ($id === null) {
-                $this->hooks[$event][] = [$callback, $parameters];
+                self::$hooks[$event][] = [$callback, $parameters];
             } else {
-                $this->hooks[$event][$id] = [$callback, $parameters];
+                self::$hooks[$event][$id] = [$callback, $parameters];
             }
-        } else {
-            throw new EventException($event, EventException::EVENT_NOT_FOUND);
-        }
 
-        return $this;
+            // 成功
+            return 0;
+        } else {
+            return EventBus::ERROR_EVENT_NOT_FOUND;
+        }
     }
 
     /**
@@ -103,35 +114,48 @@ final class EventBus
      * @param string $event 事件名称
      * @param string $id    回调函数的id
      */
-    public function unhook($event, $id = null)
+    public static function unhook($event, $id = null)
     {
+        // 如果不指定id, 则删除这个event的所有callbacks
+        // 如果指定了id, 则只删除event的指定callback
         if ($id === null) {
-            unset($this->hooks[$event]);
+            unset(self::$hooks[$event]);
         } else {
-            unset($this->hooks[$event][$id]);
+            unset(self::$hooks[$event][$id]);
         }
-        return $this;
     }
 
     /**
      * 触发一个事件，并执行挂接在这个事件上的所有回调函数。
      * 注：如果某个回调函数返回false，则不再执行后面的回调函数。
      *
-     * @param string $event 事件名称
+     * @param string $event       事件名称
+     * @param bool   $ignoreError 如果callback出错,是否继续执行后续的其它callbacks
+     *
+     * @return bool 是否成功执行了event的所有hooks
      */
-    public function trigger($event)
+    public static function trigger($event, $ignoreError = false)
     {
-        if (array_key_exists($event, $this->hooks)) {
+        if (array_key_exists($event, self::$hooks)) {
             /*
-             * 依次执行此事件上的所有回调函数
-             * 注：如果某个回调函数返回false，则不再执行后面的回调函数。
+             * 依次执行此事件上的所有回调函数.
+             * 如果某个回调函数返回false，则不再执行后面的回调函数。
              */
             foreach ($hooks as $hook) {
                 list($callback, $parameters) = $hook;
-                if (call_user_func_array($callback, $parameters) === false) {
-                    break;
+
+                if ($ignoreError) {
+                    call_user_func_array($callback, $parameters);
+                } else {
+                    // 如果执行结果是false, 不执行后续callback, 并返回失败
+                    if (call_user_func_array($callback, $parameters) === false) {
+                        return false;
+                    }
                 }
             }
         }
+
+        // 返回成功
+        return true;
     }
 }
