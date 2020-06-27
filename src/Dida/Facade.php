1768515c75c2c24987cd88660d836fef4828eedd
Macc Liu <maccliu@qq.com>
2020-06-27 19:32:16 +0800

    Facade 完成

--- src/Dida/Facade.php
+++ src/Dida/Facade.php
@@ -9,6 +9,8 @@
 
 namespace Dida;
 
+use \Dida\ServiceBus;
+
 /**
  * Facade 基类
  */
@@ -17,25 +19,96 @@ abstract class Facade
     /**
      * 版本号
      */
-    const VERSION = '20200625';
+    const VERSION = '20200627';
 
     /**
-     * @var string 绑定的Facade服务名称
+     * 服务类型常数
      */
-    protected static $didaFacadeServiceName = '';
+    const TYPE_SERVICE_BUS = "ServiceBus"; // 从ServiceBus中调用
+    const TYPE_CLASSNAME = "Classname"; // 静态类可以用这个调用
+    const TYPE_INSTANCE = "Instance"; // 具体实例可以用这个调用
 
     /**
-     * 绑定Facade的service名称
+     * @var array FacadeService链接, [token, type]
      */
-    abstract protected static function bindFacadeServiceName();
+    protected static $facadeServiceLink = [];
 
     /**
-     * 调用魔术方法
+     * 设置FacadeServiceLink
+     *
+     * @param string|object $token
+     * @param string        $type
+     * @param array         $parameters
+     * @param bool          $newInstance
+     */
+    protected static function setFacadeServiceLink(
+        $token,
+        $type = Facade::TYPE_SERVICE_BUS,
+        array $parameters = [],
+        $newInstance = false
+    ) {
+        self::$facadeServiceLink = [$token, $type, $parameters, $newInstance];
+    }
+
+    /**
+     * 调用__callStatic魔术方法
+     *
+     * @param string $name
+     * @param array  $arguments
+     *
+     * @return mixed|false 成功,返回执行结果;有错,返回false.
+     *
+     * @throws \RuntimeException Facade在执行call_user_func_array之前出错
      */
     public static function __callStatic($name, $arguments)
     {
-        $callback = [self::$didaFacadeServiceName, $name];
+        // 如果facade还没有被初始化
+        if (!self::$facadeService) {
+            throw new \RuntimeException("Facade \"" . get_called_class() . "\" not initialized.");
+        }
 
-        return call_user_func_array($callback, $arguments);
+        // 分解数组
+        list($token, $type, $parameters, $newInstance) = self::$facadeServiceLink;
+
+        // 如果是ServiceBus类型
+        if ($type === Facade::TYPE_SERVICE_BUS) {
+            if ($newInstance) {
+                $service = ServiceBus::getNew($token, $parameters);
+            } else {
+                $service = ServiceBus::get($token, $parameters);
+            }
+
+            // 如果在实例化的时候有错,抛异常
+            if (is_int($service)) {
+                throw new \RuntimeException("Getting \"$token\" service from ServiceBus fail.");
+            }
+
+            // 构造callback
+            $callback = [$service, $name];
+
+            // 返回执行结果
+            return call_user_func_array($callback, $arguments);
+        }
+
+        // 如果是实例类型
+        if ($type === Facade::TYPE_INSTANCE) {
+            // 已经是实例了,直接用就行
+            $callback = [$token, $name];
+
+            // 返回执行结果
+            return call_user_func_array($callback, $arguments);
+        }
+
+        // 如果是静态类类型
+        if ($type === Facade::TYPE_CLASSNAME) {
+            // callback在类名模式时, 实际执行的是静态方法ClassName::$name()
+            $callback = [$token, $name];
+
+            // 返回执行结果
+            return call_user_func_array($callback, $arguments);
+        }
+
+        // $type类型非法
+        throw new \RuntimeException("Illegal Facade type \"$type\"");
     }
 }
