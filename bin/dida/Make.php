8bf7b58ec74e2f499b03a587b8396d3862475041
Liu Nian <maccliu@qq.com>
2020-06-29 23:07:22 +0800

    解析method的DocDocument

--- bin/dida/Make.php
+++ bin/dida/Make.php
@@ -18,13 +18,15 @@ class Make
         $methods = $refClass->getMethods(ReflectionMethod::IS_PUBLIC + ReflectionMethod::IS_STATIC);
         foreach ($methods as $method) {
             $s = $this->buildMethod($method);
-            echo "@method static $s\n";
+            // echo "@method static $s\n";
         }
     }
 
     public function buildMethod(ReflectionMethod $method)
     {
         $name = $method->getName();
+        $comment = $method->getDocComment();
+        $this->parseMethodDocComment($comment);
 
         $params = [];
         $parameters = $method->getParameters();
@@ -34,6 +36,87 @@ class Make
         $params = implode(", ", $params);
 
         return "$name($params)";
+    }
+
+    public function parseMethodDocComment($comment)
+    {
+        $patterns = [
+            '/^\/\*\*\s{0,}/',
+            '/\s{0,}\*\/$/',
+            '/[ \t]{0,}\*[ \t]{0,}/'
+        ];
+        $result = preg_replace($patterns, '', $comment);
+        $lines = preg_split("/\n\r{0,1}/", $result);
+
+        $nodes = [];
+        $defaultNode = [
+            'tag'     => null,
+            'text'    => '',
+        ];
+        $node = $defaultNode;
+        foreach ($lines as $line) {
+            $re = "/^\@([a-zA-Z]{1,})[ \t]{0,}(.{0,})/";
+            $matches = [];
+            if (preg_match($re, $line, $matches)) {
+                $tag = $matches[1];
+                $rest = $matches[2]; // 剩余部分
+
+                // 把上一个node结束掉
+                if ($node != $defaultNode && $node !== null) {
+                    $nodes[] = $node;
+                }
+
+                // 开始这个node
+                $node = [
+                    'tag'  => $tag,
+                ];
+
+                var_dump($rest);
+                // 按照类型解析
+                switch ($tag) {
+                    case "param":
+                        $result = preg_split("/[ \t]{1,}/", $rest, 3);
+                        // 无法解析
+                        if (count($result) < 2) {
+                            $node = null;
+                            continue;
+                        }
+
+                        // 设置param
+                        $node["type"] = $result[0];
+                        $node["name"] = $result[1];
+                        if (isset($result[2])) {
+                            $node["text"] = $result[2];
+                        } else {
+                            $node["text"] = '';
+                        }
+                        break;
+
+                    case "return":
+                        $result = preg_split("/[ \t]{1,}/", $rest, 2);
+                        $node["type"] = $result[0];
+                        if (isset($result[1])) {
+                            $node["text"] = $result[1];
+                        } else {
+                            $node["text"] = '';
+                        }
+                        break;
+                }
+            } else {
+                if ($node === null) {
+                    // 上个node非法
+                } else {
+                    $node["text"] .= (($node["text"]) ? "\n" : '') . $line;
+                }
+            }
+        }
+
+        // 把上一个node结束掉
+        if ($node != $defaultNode) {
+            $nodes[] = $node;
+        }
+
+        var_dump($nodes);
     }
 
     public function buildParameter(ReflectionParameter $parameter)
