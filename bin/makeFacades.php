7940bfdc242a3fdc7aed3057b25e1190d8ac901b
Macc Liu <maccliu@qq.com>
2020-09-07 12:52:29 +0800

    Facade改为Facades

--- bin/makeFacades.php
+++ bin/makeFacades.php
@@ -21,8 +21,8 @@ $make = new Dida\Make\Facade();
 
 // 方法:
 // $make->buildFacade('参照的原始类类名', 'facade的类名', 'facade的命名空间', '要绑定的服务名', '生成的facade类名.php文件的保存路径');
-$make->buildFacade('Dida\Http\Request', 'Request', 'Dida\\Facade', 'Request', dirname(__DIR__) . "/src/Dida/Facade/Request.php");
-$make->buildFacade('Dida\Http\Response', 'Response', 'Dida\\Facade', 'Response', dirname(__DIR__) . "/src/Dida/Facade/Response.php");
-$make->buildFacade('Dida\Http\Cookie', 'Cookie', 'Dida\\Facade', 'Cookie', dirname(__DIR__) . "/src/Dida/Facade/Cookie.php");
-$make->buildFacade('Dida\Http\Session', 'Session', 'Dida\\Facade', 'Session', dirname(__DIR__) . "/src/Dida/Facade/Session.php");
-$make->buildFacade('Dida\Console\Console', 'Console', 'Dida\\Facade', 'Console', dirname(__DIR__) . "/src/Dida/Facade/Console.php");
+$make->buildFacade('Dida\Http\Request', 'Request', 'Dida\\Facades', 'Request', dirname(__DIR__) . "/src/Dida/Facades/Request.php");
+$make->buildFacade('Dida\Http\Response', 'Response', 'Dida\\Facades', 'Response', dirname(__DIR__) . "/src/Dida/Facades/Response.php");
+$make->buildFacade('Dida\Http\Cookie', 'Cookie', 'Dida\\Facades', 'Cookie', dirname(__DIR__) . "/src/Dida/Facades/Cookie.php");
+$make->buildFacade('Dida\Http\Session', 'Session', 'Dida\\Facades', 'Session', dirname(__DIR__) . "/src/Dida/Facades/Session.php");
+$make->buildFacade('Dida\Console\Console', 'Console', 'Dida\\Facades', 'Console', dirname(__DIR__) . "/src/Dida/Facades/Console.php");
