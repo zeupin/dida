b630dfefe337f7aa783b0bc5475979e13285f2b0
Macc Liu <maccliu@qq.com>
2020-09-17 19:50:00 +0800

    改进

--- src/Dida/Db/Query/WhereTrait.php
+++ src/Dida/Db/Query/WhereTrait.php
@@ -12,16 +12,6 @@ namespace Dida\Db\Query;
 trait WhereTrait
 {
     /**
-     * WHERE子句
-     *
-     * @var array [
-     *            'sql'    => ...,
-     *            'params' => [...],
-     *            ]
-     */
-    protected $clauseWHERE = [];
-
-    /**
      * WHERE的items
      */
     protected $whereItems = [];
@@ -32,11 +22,36 @@ trait WhereTrait
     protected $whereLogic = "AND";
 
     /**
-     * 按照whereItems，生成where
+     * 返回WHERE子句
      *
      * @return array ['sql'=>..., 'params'=>[...]]
      */
-    protected function buildWHERE()
+    public function clauseWHERE()
+    {
+        // 如果$whereItems为空
+        if (!$this->whereItems) {
+            return [
+                'sql'   => '',
+                'params'=> [],
+            ];
+        }
+
+        // 合并items
+        $item = $this->combineWHERE();
+
+        // 加上 WHERE
+        $item['sql'] = "WHERE " . $item['sql'];
+
+        // 返回
+        return $item;
+    }
+
+    /**
+     * 合并whereItems
+     *
+     * @return array ['sql'=>..., 'params'=>[...]]
+     */
+    protected function combineWHERE()
     {
         // 如果whereItems为空，返回空数组
         if (!$this->whereItems) {
@@ -66,6 +81,123 @@ trait WhereTrait
     }
 
     /**
+     * where条件
+     *
+     * 方式1： where($field, $op, $value)
+     * 方式2： where(array $array)
+     *              $array 关联数组。 field => value
+     *
+     * @param string|array $field 字段名，或者名字对
+     * @param string       $op    运算符
+     * @param mixed        $value 值
+     *
+     * @return \Dida\Db\Query\Query $this
+     */
+    public function where($field, $op = '=', $value = null)
+    {
+        // 如果$field是字符串
+        if (is_string($field)) {
+            $op = strtoupper($op);
+            switch ($op) {
+                case "IN":
+                    $this->whereIn($field, $value);
+                    break;
+                case "BETWEEN":
+                    $value1 = $value[0];
+                    $value2 = $value[1];
+                    $this->whereBetween($field, $value1, $value2);
+                    break;
+                default:
+                    $field = $this->quoteIdentifier($field);
+                    $item = [
+                        'sql'    => "($field $op ?)",
+                        'params' => [$value],
+                    ];
+                    $this->whereItems[] = $item;
+            }
+
+            // 完成
+            return $this;
+        }
+
+        // 如果$field是数组
+        if (is_array($field)) {
+            foreach ($field as $f => $v) {
+                $this->where($f, $op, $v);
+            }
+
+            // 完成
+            return $this;
+        }
+
+        // 其它情况抛异常
+        throw new \Exception('Invalid parameter in ' . __METHOD__ . '()');
+    }
+
+    /**
+     * WHERE: IN
+     *
+     * @param string $field  字段
+     * @param mixed  $values 值
+     *
+     * @return \Dida\Db\Query\Query $this
+     */
+    public function whereIn($field, array $values)
+    {
+        $field = $this->quoteIdentifier($field);
+        $marks = substr(str_repeat('?,'), 0, -1);
+        $item = [
+            'sql'    => "($field IN ($marks))",
+            'params' => [$value1, $value2],
+        ];
+        $this->whereItems[] = $item;
+
+        // 完成
+        return $this;
+    }
+
+    /**
+     * WHERE: BETWEEN
+     *
+     * @param string $field 字段
+     * @param mixed  $value 值
+     * @param string $op    运算符
+     *
+     * @return \Dida\Db\Query\Query $this
+     */
+    public function whereBetween($field, $value1, $value2)
+    {
+        $field = $this->quoteIdentifier($field);
+        $item = [
+            'sql'    => "($field BETWEEN ? AND ?)",
+            'params' => [$value1, $value2],
+        ];
+        $this->whereItems[] = $item;
+
+        // 完成
+        return $this;
+    }
+
+    /**
+     * WHERE: 对于复杂WHERE表达式，提供这个函数直接设置
+     *
+     * @param string $sql    SQL
+     * @param array  $params 参数
+     *
+     * @return \Dida\Db\Query\Query $this
+     */
+    public function whereRaw($sql, array $params)
+    {
+        $this->whereItems[] = [
+            'sql'   => $sql,
+            'params'=> $params,
+        ];
+
+        // 完成
+        return $this;
+    }
+
+    /**
      * 设置 $this->wherLogic
      *
      * @return \Dida\Db\Query\Query $this
@@ -74,7 +206,7 @@ trait WhereTrait
     {
         // 如果whereItems不为空，先把前面设置的所有WHERE做一个打包
         if ($this->whereItems) {
-            $item = $this->buildWHERE();
+            $item = $this->combineWHERE();
             $this->whereItems = [$item];
         }
 
@@ -103,74 +235,5 @@ trait WhereTrait
     public function whereAnd()
     {
         return $this->whereLogic('AND');
-    }
-
-    /**
-     * where条件
-     *
-     * @param string $field 字段
-     * @param mixed  $value 值
-     * @param string $op    运算符
-     *
-     * @return \Dida\Db\Query\Query $this
-     */
-    public function where($field, $value, $op = '=')
-    {
-        $op = strtoupper($op);
-        switch ($op) {
-            case "IN":
-                $this->whereIn($field, $value);
-                break;
-            case "BETWEEN":
-                $value1 = $value[0];
-                $value2 = $value[1];
-                $this->whereBetween($field, $value1, $value2);
-                break;
-            default:
-                $field = $this->quoteIdentifier($field);
-                $item = [
-                    'sql'    => "($field $op ?)",
-                    'params' => [$value],
-                ];
-                $this->whereItems[] = $item;
-        }
-    }
-
-    /**
-     * WHERE: IN
-     *
-     * @param string $field  字段
-     * @param mixed  $values 值
-     *
-     * @return \Dida\Db\Query\Query $this
-     */
-    public function whereIn($field, array $values)
-    {
-        $field = $this->quoteIdentifier($field);
-        $marks = substr(str_repeat('?,'), 0, -1);
-        $item = [
-            'sql'    => "($field IN ($marks))",
-            'params' => [$value1, $value2],
-        ];
-        $this->whereItems[] = $item;
-    }
-
-    /**
-     * WHERE: BETWEEN
-     *
-     * @param string $field 字段
-     * @param mixed  $value 值
-     * @param string $op    运算符
-     *
-     * @return \Dida\Db\Query\Query $this
-     */
-    public function whereBetween($field, $value1, $value2)
-    {
-        $field = $this->quoteIdentifier($field);
-        $item = [
-            'sql'    => "($field BETWEEN ? AND ?)",
-            'params' => [$value1, $value2],
-        ];
-        $this->whereItems[] = $item;
     }
 }
