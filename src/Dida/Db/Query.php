f34a011e0b30887a110be1b810ad16673a22f815
Macc Liu <maccliu@qq.com>
2020-09-25 00:46:03 +0800

    dev

--- src/Dida/Db/Query.php
+++ src/Dida/Db/Query.php
@@ -1260,6 +1260,58 @@ SQL;
     }
 
     /**
+     * UPDATE操作
+     *
+     * @param array        $row    要更新的数据项
+     * @param array|string $wheres 条件。参见 $this->clauseWHERE()。
+     *
+     * @return \Dida\Db\ResultSet
+     */
+    public function update(array $row, $wheres = null)
+    {
+        // 处理$row
+        if ($row) {
+            $this->dataItems = $row;
+        } else {
+            throw new \Exception('Invalid parameter value $row: ' . var_export($row, true));
+        }
+
+        // 处理$wheres
+        if (!$wheres) {
+        } elseif (is_array($wheres)) {
+            $this->where($wheres);
+        } elseif (is_string($wheres)) {
+            $this->whereRaw($wheres, []);
+        } else {
+            throw new \Exception('Invalid parameter value $wheres.' . var_export($row, true));
+        }
+
+        $_table = $this->sqlMainTable();
+
+        // set子句
+        $_set = $this->clauseSET();
+
+        // where子句
+        $_where = $this->clauseWHERE();
+
+        // 构造sql
+        $sql = <<<SQL
+UPDATE $_table
+{$_set["sql"]}
+{$_where["sql"]}
+SQL;
+
+        // 构造params
+        $params = array_merge($_set["params"], $_where["params"]);
+
+        // 执行
+        $rs = $this->driver->execWrite($sql, $params);
+
+        // 返回 Dida\Db\ResultSet
+        return $rs;
+    }
+
+    /**
      * DELETE
      *
      * @param array $wheres 条件。参见 $this->clauseWHERE()。
@@ -1370,57 +1422,5 @@ SQL;
 
         // 成功，返回count
         return intval($rs->getColumn("count"));
-    }
-
-    /**
-     * UPDATE操作
-     *
-     * @param array        $row    要更新的数据项
-     * @param array|string $wheres 条件。参见 $this->clauseWHERE()。
-     *
-     * @return \Dida\Db\ResultSet
-     */
-    public function update(array $row, $wheres = null)
-    {
-        // 处理$row
-        if ($row) {
-            $this->dataItems = $row;
-        } else {
-            throw new \Exception('Invalid parameter value $row: ' . var_export($row, true));
-        }
-
-        // 处理$wheres
-        if (!$wheres) {
-        } elseif (is_array($wheres)) {
-            $this->where($wheres);
-        } elseif (is_string($wheres)) {
-            $this->whereRaw($wheres, []);
-        } else {
-            throw new \Exception('Invalid parameter value $wheres.' . var_export($row, true));
-        }
-
-        $_table = $this->sqlMainTable();
-
-        // set子句
-        $_set = $this->clauseSET();
-
-        // where子句
-        $_where = $this->clauseWHERE();
-
-        // 构造sql
-        $sql = <<<SQL
-UPDATE $_table
-{$_set["sql"]}
-{$_where["sql"]}
-SQL;
-
-        // 构造params
-        $params = array_merge($_set["params"], $_where["params"]);
-
-        // 执行
-        $rs = $this->driver->execWrite($sql, $params);
-
-        // 返回 Dida\Db\ResultSet
-        return $rs;
     }
 }
